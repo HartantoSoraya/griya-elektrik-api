6cc0435e4241ce1c9fc9aa2aa9d48a9743bb7593
DoctorAcc22 <doctoracc22@gmail.com>
2024-03-04 00:23:06 +0700

    update

--- app/Http/Requests/UpdateBranchRequest.php
+++ app/Http/Requests/UpdateBranchRequest.php
@@ -29,23 +29,12 @@ class UpdateBranchRequest extends FormRequest
             'is_main' => 'required|boolean',
             'is_active' => 'required|boolean',
             'branch_images' => 'nullable|array',
-            'branch_images.*.id' => 'nullable|string|exists:branch_images,id',
-            'branch_images.*.image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'branch_images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ];
     }
 
     public function prepareForValidation()
     {
-        if ($this->has('branch_images')) {
-            $branchImages = $this->branch_images;
-            foreach ($branchImages as $index => $branchImage) {
-                if (! isset($branchImage['id'])) {
-                    $branchImages[$index]['id'] = null;
-                }
-            }
-            $this->merge(['branch_images' => $branchImages]);
-        } else {
-            $this->merge(['branch_images' => []]);
-        }
+
     }
 }
