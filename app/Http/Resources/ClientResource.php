3e54373e0ca1429446a3db04893ee3c050a6137b
DoctorAcc22 <doctoracc22@gmail.com>
2024-03-06 17:07:31 +0700

    update

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -18,7 +18,7 @@ class ClientResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'logo' => $this->logo,
-            'logo_url' => $this->logo_url,
+            'logo_url' => $this->logo ? asset('storage/'.$this->logo) : '',
             'url' => $this->url,
         ];
     }
