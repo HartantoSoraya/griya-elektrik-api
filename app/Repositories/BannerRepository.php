ca8143cc14fbd35d74de0386aa8f27aac5fa3403
Hugo YAW <hugoyaw22@gmail.com>
2024-09-13 20:16:01 +0700

    banner, now can compress stored images

--- app/Repositories/BannerRepository.php
+++ app/Repositories/BannerRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Helpers\ImageHelper\ImageHelper;
 use App\Interfaces\BannerRepositoryInterface;
 use App\Models\Banner;
 use Illuminate\Support\Facades\Storage;
@@ -21,11 +22,30 @@ class BannerRepository implements BannerRepositoryInterface
     public function createBanner($data)
     {
         $banner = new Banner();
-        $banner->desktop_image = $data['desktop_image']->store('assets/banners', 'public');
-        $banner->mobile_image = $data['mobile_image']->store('assets/banners', 'public');
+        // $banner->desktop_image = $data['desktop_image']->store('assets/banners', 'public');
+        // $banner->mobile_image = $data['mobile_image']->store('assets/banners', 'public');
+
+        $banner->desktop_image = $this->saveImage($data['desktop_image']);
+        $banner->mobile_image = $this->saveImage($data['mobile_image']);
         $banner->save();
 
         return $banner;
+    }
+
+    private function saveImage($image)
+    {
+        if ($image) {
+            $path = $image->store('assets/banners', 'public');
+            // $storagePath = storage_path('app/public/'.$path);
+
+            $storagePath = Storage::disk('public')->path($path);
+            $imageHelper = new ImageHelper();
+            $imageHelper->resizeImage($storagePath, $storagePath, 1500, 1500);
+
+            return $path;
+        } else {
+            return null;
+        }
     }
 
     public function updateBanner($data, $id)
@@ -33,10 +53,12 @@ class BannerRepository implements BannerRepositoryInterface
         $banner = Banner::find($id);
 
         if ($data['desktop_image']) {
-            $banner->desktop_image = $this->updateImage($banner->desktop_image, $data['desktop_image']);
+            // $banner->desktop_image = $this->updateImage($banner->desktop_image, $data['desktop_image']);
+            $banner->desktop_image = $this->updateImage($data['desktop_image'], $banner->desktop_image);
         }
         if ($data['mobile_image']) {
-            $banner->mobile_image = $this->updateImage($banner->mobile_image, $data['mobile_image']);
+            // $banner->mobile_image = $this->updateImage($banner->mobile_image, $data['mobile_image']);
+            $banner->mobile_image = $this->updateImage($data['mobile_image'], $banner->mobile_image);
         }
 
         $banner->save();
@@ -44,17 +66,37 @@ class BannerRepository implements BannerRepositoryInterface
         return $banner;
     }
 
+    public function updateImage($image, $oldImagePath)
+    {
+        if ($image) {
+            if ($oldImagePath) {
+                Storage::disk('public')->delete($oldImagePath);
+            }
+
+            $path = $image->store('assets/banners', 'public');
+
+            // $storagePath = storage_path('app/public/'.$path);
+            $storagePath = Storage::disk('public')->path($path);
+            $imageHelper = new ImageHelper();
+            $imageHelper->resizeImage($storagePath, $storagePath, 1500, 1500);
+
+            return $path;
+        } else {
+            return $oldImagePath;
+        }
+    }
+
     public function deleteBanner($id)
     {
         return Banner::destroy($id);
     }
 
-    private function updateImage($oldImage, $newImage): string
-    {
-        if ($oldImage !== $newImage) {
-            Storage::disk('public')->delete($oldImage);
-        }
+    // private function updateImage($oldImage, $newImage): string
+    // {
+    //     if ($oldImage !== $newImage) {
+    //         Storage::disk('public')->delete($oldImage);
+    //     }
 
-        return $newImage->store('assets/banners', 'public');
-    }
+    //     return $newImage->store('assets/banners', 'public');
+    // }
 }
